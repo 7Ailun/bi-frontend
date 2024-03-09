d44cc1d034e0b67084ca7578066c5915b5fc8110
Allen <993013755@qq.com>
2024-03-10 00:02:54 +0800

    first

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,10 @@
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
-import { useIntl } from '@umijs/max';
+import '@umijs/max';
 import React from 'react';
-
 const Footer: React.FC = () => {
-  const intl = useIntl();
-  const defaultMessage = intl.formatMessage({
-    id: 'app.copyright.produced',
-    defaultMessage: '蚂蚁集团体验技术部出品',
-  });
-
+  const defaultMessage = '进击的艾伦出品';
   const currentYear = new Date().getFullYear();
-
   return (
     <DefaultFooter
       style={{
@@ -20,26 +13,25 @@ const Footer: React.FC = () => {
       copyright={`${currentYear} ${defaultMessage}`}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
+          key: '语雀',
+          title: '艾伦语雀知识库',
+          href: 'https://www.yuque.com/yuhangyuan-vp13i',
           blankTarget: true,
         },
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          href: 'https://github.com/7Ailun?tab=repositories',
           blankTarget: true,
         },
         {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          key: 'Blog',
+          title: '艾伦的编程小屋',
+          href: 'https://ailuncc.top/',
           blankTarget: true,
         },
       ]}
     />
   );
 };
-
 export default Footer;
